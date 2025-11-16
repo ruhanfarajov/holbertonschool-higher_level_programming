d1adc3abfa0694efad605354a8b8f4b6c45a2728
ruhanfarajov <ruhanfarajov@gmail.com>
2025-11-16 17:22:04 +0000

    fjasALKS;SJKA

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 for i in range (122, 96, -1):
-    if i % 2 == 0:
-        print(chr(i), end="")
+    if i % 2 != 0:
+        f = i-32
     else:
-        print(chr(i-32), end="")
+        f = i
+    print(chr(f), end="")
