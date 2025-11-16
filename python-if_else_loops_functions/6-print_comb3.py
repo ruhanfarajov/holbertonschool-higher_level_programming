0146447ea9f0257af166c6abb1caa6a66a04cbf5
ruhanfarajov <ruhanfarajov@gmail.com>
2025-11-16 15:39:07 +0000

    keld;s

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i == 99:
+    if i == 89:
         print("{:2d}".format(i))
     else:
-        for j in range(i, 100):
-            if str(i) == str(j)[::-1]:
+        for j in range(i+1, 100):
+            if str(i) == str(j)[::-1] or "0" + str(i) == str(j)[::-1]:
                 print("{:02d}".format(i), end=", ")
