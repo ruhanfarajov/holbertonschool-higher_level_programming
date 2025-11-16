2a273cbd968bed295a3313436b2ca40cfb4a5414
ruhanfarajov <ruhanfarajov@gmail.com>
2025-11-16 16:23:34 +0000

    salamaqa

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    s = ""
+    for i in str:
+        if ord(i) >= 97 and ord("i") <= 122:
+            ch_add = ord(i) - 32
+            s += chr(ch_add)
+        else:
+            s += i
+    print(s)
