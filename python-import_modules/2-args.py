9dad0c9f12bd6d6fccd43533cc1845cf95e3e330
ruhanfarajov <ruhanfarajov@gmail.com>
2025-11-17 15:30:03 +0000

    wwDNMdsm.m,

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,13 @@
 
 import sys
 
-lst = sys.argv[1:]
 
-for i in range(0, len(lst)):
-    print("{}. {}".format(i+1, lst[i]))
+def main():
+    lst = sys.argv[1:]
+    print("{} arguments:".format(len(lst)))
+    for i in range(0, len(lst)):
+        print("{}. {}".format(i+1, lst[i]))
+
+
+if __name__ == "__main__":
+    main()
