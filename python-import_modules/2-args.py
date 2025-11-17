35b39741bc570f5491ba5464035df80f78904c86
ruhanfarajov <ruhanfarajov@gmail.com>
2025-11-17 15:30:52 +0000

    jkasxklAKSmx.a

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,7 +7,7 @@ def main():
     lst = sys.argv[1:]
     print("{} arguments:".format(len(lst)))
     for i in range(0, len(lst)):
-        print("{}. {}".format(i+1, lst[i]))
+        print("{}: {}".format(i+1, lst[i]))
 
 
 if __name__ == "__main__":
