f1c12b4f0f94e2c69e425bd238c3452a6182a8b4
ruhanfarajov <ruhanfarajov@gmail.com>
2025-11-17 15:33:35 +0000

    wkldksj

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,10 @@ import sys
 
 def main():
     lst = sys.argv[1:]
-    print("{} arguments:".format(len(lst)))
+    if len(lst) == 1:
+        print("{} argument:".format(len(lst)))
+    else:
+        print("{} arguments:".format(len(lst)))
     for i in range(0, len(lst)):
         print("{}: {}".format(i+1, lst[i]))
 
