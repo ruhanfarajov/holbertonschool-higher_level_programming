247ea84be441b9e6efe27005fe1b3f1a7f48abd0
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-18 04:51:57 -0500

    alindi

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import sys
+from calculator_1 import add, sub, div, mul
+
+
+def calc(a, op, b):
+    result = 0
+    if op == "+":
+        result = add(a, b)
+    elif op == "-":
+        result = sub(a, b)
+    elif op == "*":
+        result = mul(a, b)
+    elif op == "/":
+        result = div(a, b)
+    return result
+
+
+def main(lst):
+    operands = ["+", "-", "*", "/"]
+    if len(lst) != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    elif lst[1] not in operands:
+        print("Unknown operator. Available operators: +, -, * and /")
+    else:
+        answer = calc(int(lst[0]),  lst[1], int(lst[2]))
+        print("{} {} {} = {}".format(lst[0],  lst[1],  lst[2], answer))
+
+
+lst = sys.argv[1:]
+
+if __name__ == "__main__":
+    main(lst)
