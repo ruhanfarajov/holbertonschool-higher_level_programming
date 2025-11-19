0ec009b9143f8f6dceffac63e109a842511e3a87
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-19 04:34:47 -0500

    sldklk

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+    new_list = []
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    else:
+        for i in range(0, len(my_list)):
+            if i == idx:
+                new_list.append(element)
+            else:
+                new_list.append(my_list[i])
+        return new_list
