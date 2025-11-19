7ded7517c0756d78fd77f4743465894d7d38eefc
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-19 08:43:12 -0500

    ;llkdsjkql

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for i in range(0, len(matrix)):
-        for j in range(0, len(matrix[i])):
-            if j == len(matrix[i]) - 1:
-                print("{:d}".format(matrix[i][j]), end='\n')
-            else:
-                print("{:d}".format(matrix[i][j]), end=' ')
+    if len(matrix) == 0:
+        print("{:d}".format(1))
+    else:
+        for i in range(0, len(matrix)):
+            for j in range(0, len(matrix[i])):
+                if j == len(matrix[i]) - 1:
+                    print("{:d}".format(matrix[i][j]), end='\n')
+                else:
+                    print("{:d}".format(matrix[i][j]), end=' ')
