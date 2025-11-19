aee4b5b13062730102bcfa51034948750483e4c3
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-19 17:00:04 -0500

    hot fix on the matrix[i]

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,6 +4,6 @@ def square_matrix_simple(matrix=[]):
     new_matrix = []
     for i in range(len(matrix)):
         new_matrix.append([])
-        for j in range(len(matrix)):
+        for j in range(len(matrix[i])):
             new_matrix[i].append(matrix[i][j]**2)
     return new_matrix
