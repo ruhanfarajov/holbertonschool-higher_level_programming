9cf70b6de838d7f9a4dc073929c203157413ee30
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-19 19:44:47 -0500

    kjd,bdkjaslk;

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,6 +4,8 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     lst = list(a_dictionary)
+    if len(lst) == 0:
+        return None
     best = lst[0]
     for i in lst:
         if a_dictionary[i] > a_dictionary[best]:
