6bbb86a284d365a0f0c39a27b6ac7e7001f19e8a
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-22 11:34:35 -0500

    salam 12345

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         nb_print = 0
         s = ''
         for i in my_list:
-            if nb_print > x: 
+            if nb_print >= x:
                 break
             s += str(i)
             nb_print += 1
