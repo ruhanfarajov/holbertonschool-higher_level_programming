94e8a2bb30c98ee107a4865204fa5e6518ef44c1
ruhanfarajov <ruhanfarajov@gmail.com>
2025-11-22 09:39:36 +0400

    Update 9-multiply_by_2.py

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -5,7 +5,7 @@ def multiply_by_2(a_dictionary):
     if len(lst) == 0:
         return a_dictionary
     else:
-        new_dict={}
+        new_dict = {}
         for i in lst:
             new_dict[i] = a_dictionary[i] * 2
         return new_dict
