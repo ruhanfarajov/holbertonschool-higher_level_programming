bae5b64399d7cf61688dd283eb3d49416e908b69
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-24 12:05:18 -0500

    fslkkld

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,1 +1,39 @@
 #!/usr/bin/python3
+"This defines a rectangle with witdh and height"
+
+
+class Rectangle:
+    "this add methods to this class"
+
+    def __init__(self, width=0, height=0):
+        "Let's set some variables"
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        "this is width getter"
+        return self.__width
+
+    @property
+    def height(self):
+        "this is height getter"
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        "the property setter for width"
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        "this is property setter for the height"
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
