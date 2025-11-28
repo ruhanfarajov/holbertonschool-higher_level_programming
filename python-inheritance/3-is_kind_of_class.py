072d5392f10cd55daea29ddf726622e8db5bbf79
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-28 08:08:41 -0500

    dksKLDS

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+'''Making America Great Again by doing TRUMP's missions'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''Making America Great Again by doing TRUMP's missions'''
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
