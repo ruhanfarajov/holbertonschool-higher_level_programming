63ecd8ebee6fce4b5a37260d561f6b4702d97d36
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-28 08:55:53 -0500

    SASha

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Making America Great Again by doing TRUMP's missions'''
+
+
+class BaseGeometry():
+    '''Making America Great Again by doing TRUMP's missions'''
+    def area(self):
+        '''raises and exception mesaage that area is not implemented'''
+        raise Exception("area() is not implemented")
