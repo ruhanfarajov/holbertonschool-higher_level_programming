1c33ebda1025fff10e5dd05f50e73052cb4f970a
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-28 07:54:14 -0500

    lkasa;

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+'''Making America Great Again by doing TRUMP's missions'''
+
 
 def is_same_class(obj, a_class):
+    '''Making America Great Again by doing TRUMP's missions'''
     if isinstance(obj, a_class):
         return True
     else:
