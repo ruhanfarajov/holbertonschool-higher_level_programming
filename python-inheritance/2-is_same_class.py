2478c232fa25d3ce8e27da64eacd6e85bbe8b91d
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-28 08:01:26 -0500

    hoifhWODC

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,7 +4,7 @@
 
 def is_same_class(obj, a_class):
     '''Making America Great Again by doing TRUMP's missions'''
-    if isinstance(obj, a_class):
+    if type(obj) is a_class:
         return True
     else:
         return False
