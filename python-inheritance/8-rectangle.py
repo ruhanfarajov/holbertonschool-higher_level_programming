f6711be7d5d2f9a5fbdaca1090d71c26263687d3
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-28 09:34:51 -0500

    dsdIJDJCKBHK

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Making America Great Again by doing TRUMP's missions'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''Making America Great Again by doing TRUMP's missions'''
+
+    def __init__(self, width=0, height=0):
+        '''Making America Great Again by doing TRUMP's missions'''
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
