6a53bd7f353dc807bf36a891630c13bb2548bc57
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-28 08:17:01 -0500

    hdaijsakd

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Making America Great Again by doing TRUMP's missions'''
+
+
+def inherits_from(obj, a_class):
+    '''Making America Great Again by doing TRUMP's missions'''
+    if isinstance(obj, a_class) and type(obj) is not a_class:
+        return True
+    else:
+        return False
