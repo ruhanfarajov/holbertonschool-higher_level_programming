9ec20a902bbd35f9eec6f54b77f9d7969716712c
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-28 06:47:57 -0500

    kdssldsk

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
+'''Making America Great Again'''
+
 
 class MyList():
+    '''I am not working'''
 
     def __init__(self, data=[]):
+        '''initialize the variable'''
         self.data = list(data)
 
     def print_sorted(self):
+        '''sort the list'''
         self.data.sort()
 
     def append(self, value):
+        '''appends the list a value'''
         self.data.append(value)
 
     def __repr__(self):
+        '''return my _list'''
         return repr(self.data)
