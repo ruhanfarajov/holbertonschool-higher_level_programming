d1631edeb00f173cde5310f99e79044d1965988b
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-29 04:54:48 -0500

    dsknkdsl

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'"This is writing the file'''
+
+
+def write_file(filename='', text=''):
+    '''This is wiriting thw text to the file and returns the count'''
+
+    with open(filename, 'a', encoding='utf-8') as file:
+        file.append(text)
+    return len(text)
