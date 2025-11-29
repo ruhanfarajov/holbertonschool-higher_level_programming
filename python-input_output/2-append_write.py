599528d82b34c1919a77c00d3b7abcd402f5f2ba
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-29 04:58:10 -0500

    ldskjkkjkqpaodlk

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-'"This is writing the file'''
+'"This is writing the file and MAGA'''
 
 
 def append_write(filename='', text=''):
     '''This is wiriting thw text to the file and returns the count'''
 
     with open(filename, 'a', encoding='utf-8') as file:
-        file.append(text)
+        file.write(text)
     return len(text)
