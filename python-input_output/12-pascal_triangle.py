2e60b8a69fdd43f027db1b6cb4620b8376d615ab
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-29 12:35:17 -0500

    wuwehdkwd

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/sur/bin/python3
+'''This is intended to create pascal triangle'''
+
+
+def pascal_triangle(n):
+    """Returns a list of lists representing Pascal's triangle of n."""
+    if n <= 0:
+        return []
+
+    triangle = []
+    row = [1]
+    triangle.append(row)
+
+    for _ in range(1, n):
+        prev = row
+        row = [1]
+
+        for i in range(len(prev) - 1):
+            row.append(prev[i] + prev[i + 1])
+
+        row.append(1)
+
+        triangle.append(row)
+
+    return triangle
