86a78ef665788b8b53201a88b6fab92130a9120f
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-29 14:41:27 -0500

    kdjDJLlJC JC B

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''this is doing america grreat again'''
+
+import csv
+import json
+
+
+def convert_csv_to_json(data, filename):
+    try:
+        data = []
+
+        with open(filename, 'r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                data.append(row)
+
+        with open(data.json, 'w', encoding='utf-8') as file:
+            file.write(json.dumps(data))
+
+        return True
+    except FileNotFoundError:
+        return False
+
+    except Exception:
+        return False
