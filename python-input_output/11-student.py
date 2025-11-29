258721688b7435a51f9c6a8947094476baa3120e
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-29 10:49:42 -0500

    woewlkfefk

---
+++ python-input_output/11-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''
+SALAM THIS MODULE CAN BE DOCUMENTD AS SCRIPT
+'''
+
+
+class Student():
+    '''This is a student class created by user'''
+
+    def __init__(self, first_name, last_name, age):
+        '''initializing variables here'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, my_list=None):
+        '''
+        this just return json formatted data
+        '''
+        dict_ = {}
+        if my_list is None:
+            return self.__dict__
+        elif len(my_list) == 0:
+            return dict_
+        for i in my_list:
+            for j in self.__dict__:
+                if i == j:
+                    dict_[i] = self.__dict__[j]
+        return dict_
+
+    def reload_from_json(self, json):
+        for key in self.__dict__:
+            for key2 in json:
+                if key == key2:
+                    self[key] = json[key]
