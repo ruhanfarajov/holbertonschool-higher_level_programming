3ecb9f2f02dc5f1ebb7f634d295f9f27a284bcf4
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-29 05:58:07 -0500

    mnnsdkakldkl

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,13 +6,18 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-loadd-from_json_file').load_from_json_file
 
-filename = 'add_item.js'
 
-if Path(filename).exists():
-    items = load_from_json_file(filename)
-else:
-    items = []
+def add_item():
+    '''This is adding items to the list of jsj'''
+    filename = 'add_item.js'
+    if Path(filename).exists():
+        items = load_from_json_file(filename)
+    else:
+        items = []
 
-items.extend(sys.argv[1:])
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, filename)
 
-save_to_json_file(items, filename)
+
+if __name__ = "__main__":
+    add_item()
