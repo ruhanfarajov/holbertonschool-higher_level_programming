117161f1775a0ade3c3eae9379774425df39676b
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-30 10:20:25 -0500

    lmfmdsl;SKLAFD

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- this is generating htbn-usa database and add cities table inside it
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+	id INT NOT NULL UNIQUE AUTO_INCREMENT,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (state_id) REFERENCES states(id) );
