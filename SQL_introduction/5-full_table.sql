05be8dbcfaee88f768af6b3b6b9dd4ef7298dff3
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-29 21:34:47 -0500

    kncxcldsk

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,10 +1,2 @@
 -- create table if not exists
-DROP TABLE IF EXISTS first_table
-
-CREATE TABLE IF NOT EXISTS first_table (
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(256) DEAULT NULL,
-	PRIMARY KEY (d)
-)
-
-SELECT * FROM first_table
+SHOW CREATE TABLE first_table
