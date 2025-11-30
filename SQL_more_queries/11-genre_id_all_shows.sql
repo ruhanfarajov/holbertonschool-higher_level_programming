1ed78fbb091357f4f80a122719d3a8d844da0379
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-30 12:09:10 -0500

    csdjalkn

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -4,7 +4,9 @@ SELECT
         s.title, g.genre_id
 FROM
         tv_shows AS s
-FULL JOIN
+LEFT JOIN
         tv_show_genres AS g
 ON
         s.id =g.show_id 
+ORDER BY
+	s.titile, g.genre_id ASC;
