5700a27c25bff9eb7ae7db010bab0dd5ebd1d949
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-30 10:43:03 -0500

    dsfdmnskkls

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,11 @@
 -- this script is my first filter in SQL
-SELECT * FROM cities WHERE cities.state_id = states.id
+SELECT 
+	cities.*
+FROM 
+	cities,
+	states
+WHERE
+	cities.state_id = states.id && states.name = 'California'
+ORDER BY
+	cities.id
+	ASC
