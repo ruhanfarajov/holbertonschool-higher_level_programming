130a341f0402c7adc77211790c28e2d23e44b778
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-30 10:13:07 -0500

    sdldkalkdnk

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- this is generating the database and table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT NOT NULL AUTO_INCREMENT UNIQUE,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id) );
