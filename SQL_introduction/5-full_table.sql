29dd939c108993e9cb69ecd1d0fbecb335c08f89
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-29 21:32:58 -0500

    dskodsk

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,10 @@
 -- create table if not exists
+DROP TABLE IF EXISTS first_table
+
+CREATE TABLE IF NOT EXISTS first_table (
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(256) DEAULT NULL,
+	PRIMARY KEY (d)
+)
+
 SELECT * FROM first_table
