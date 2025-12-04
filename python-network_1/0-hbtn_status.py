2e45556be993c0e5995a85e6ecf682f026f7737a
ruhanfarajov <ruhanfarajov@gmail.com>
2025-12-04 09:22:47 +0000

    jhfhajfv

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,18 +1,35 @@
 #!/usr/bin/python3
-'''this is importing so good things'''
+
+#!/usr/bin/python3
+"""
+0-hbtn_status.py
+
+Fetches the URL https://intranet.hbtn.io/status using urllib,
+then displays"""
+
+
 import urllib.request
 
+def fetch_status(url):
+    """
+    Fetch the given URL and return the raw response body (bytes).
+    """
+    with urllib.request.urlopen(url) as response:
+        return response.read()
 
-def fetch(url):
-    '''fethcing the document'''
-    req = urlib.request.Request(
-            url,
-            headers={"cfclearance: "true}
-    )
+def main():
+    url = "https://intranet.hbtn.io/status"
+    body = fetch_status(url)
 
-    with urlib.request.urlopen(req) as response:
-        return response.read(-).decode('utf-8')
+    # Print result in required format
+    print("Body response:")
+    print("    - type: {}".format(type(body)))
+    print("    - content: {}".format(body))
+    try:
+        text = body.decode('utf-8')
+    except Exception:
+        text = body.decode('utf-8', errors='replace')
+    print("    - utf8 content: {}".format(text))
 
 if __name__ == "__main__":
-    '''safer way'''
-    print(fetch('https://intranet.hbtn.io/status'))
+    main()
