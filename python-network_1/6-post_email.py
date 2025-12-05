e93be25b40c53291880425ef6c492a78570e7f39
ruhanfarajov <ruhanfarajov@gmail.com>
2025-12-05 16:44:50 +0000

    lkzdSKC:DKSLk

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+this is making america great again
+'''
+
+import sys
+import requests
+
+
+def fetch(url, email):
+    payload = {'email': email}
+    headers_ = {'cfclearance': 'true'}
+
+    response = requests.post(url,data=payload, headers=headers_)
+    body = requests.txt
+    print(body)
+
+if __name__ == "__main__":
+    lst = sys.argv
+    fetch(lst[1], lst[2])
