c0bdfc8e3d92ebf894f9e47e0b6fb39907695f15
ruhanfarajov <ruhanfarajov@gmail.com>
2025-12-05 14:41:02 +0000

    wiodWLjC<AMD XVcx..x

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-i am doing everything 
+I am doing everything
 '''
 import urllib.request
 import urllib.parse
@@ -10,7 +10,7 @@ import sys
 def post(url, email):
     data = {"email": email}
     encoded = urllib.parse.urlencode(data).encode('utf-8')
-    req = urllib.request.Request(url,encoded)
+    req = urllib.request.Request(url, encoded)
     req.add_header('cfclearance', 'true')
 
     with urllib.request.urlopen(req) as response:
