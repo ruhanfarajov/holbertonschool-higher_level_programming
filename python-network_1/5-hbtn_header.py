b33b960f2a326d1b4c19f89cdc7af6bf122e03cf
ruhanfarajov <ruhanfarajov@gmail.com>
2025-12-05 16:10:16 +0000

    JDSJKLSJCKLAs

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+I am doing well
+'''
+
+import requests
+import sys
+
+def fetch(url):
+    html = requests.get(url)
+    body = html.headers
+    print(body)
+
+if __name__ == "__main__":
+    try:
+        lst = sys.argv
+        url = lst[1]
+        fetch(url)
+    except Exception as e:
+        print("Error happened: {}".format(e))
