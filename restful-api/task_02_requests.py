297518b8957ff0f5ed5c5fbcc6b25505c8ec9550
ruhanfarajov <ruhanfarajov@gmail.com>
2025-12-06 20:59:43 +0400

    Update task_02_requests.py

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -7,16 +7,16 @@ import csv
 
 
 def fetch_and_print_posts():
-    url = "https://jsonplaceholder.typicode.com/"
+    url = "https://jsonplaceholder.typicode.com/todos/1"
     response = requests.get(url)
-    data = response.json()
     if response.status_code == 200:
+        data = response.json()
         for i in data:
             print(data[i])
 
 
 def fetch_and_save_posts():
-    url = "https://jsonplaceholder.typicode.com/"
+    url = "https://jsonplaceholder.typicode.com/todos/1"
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
