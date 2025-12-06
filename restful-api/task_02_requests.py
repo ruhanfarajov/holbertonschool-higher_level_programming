5bdc1a32ed8c74d53c833692379e2335fec5ddd6
ruhanfarajov <ruhanafarajov@gmail.com>
2025-12-06 11:25:56 -0500

    jkchajkadhckh

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+This is making america great again
+'''
+import requests
+import csv
+
+
+def fetch_and_print_post():
+    url = "https://jsonplaceholder.typicode.com/"
+    response = requests.get(url)
+    status = response.status_code()
+   if status == 200:
+       dict_ = response.json()
+       for i in dict_:
+           print(dict_[i])
+
+def fetch_and_save_post():
+    url = "https://jsonplaceholder.typicode.com/"
+    response = requests.get(url)
+    status = response.status_code()
+    if status == 200:
+        dict_ = response.json
+        with open('posts.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["key", "value"])
+            for key, value in dict_.items():
+                writer.writerow([key, value])
