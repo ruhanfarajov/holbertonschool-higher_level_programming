a15eef6988dcc58a4e7b6983043a05ca2042f1fc
ruhanfarajov <ruhanfarajov@gmail.com>
2025-12-06 21:38:16 +0400

    Update task_02_requests.py

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -7,19 +7,20 @@ import csv
 
 
 def fetch_and_print_posts():
-    url = "https://jsonplaceholder.typicode.com/todos/1"
+    url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
+    print(response.status_code)
     if response.status_code == 200:
-        print("Status Code: {}".format(response.status_code))
         data = response.json()
+        print(data)
         for i in data:
-            print(data[i])
+            print(i['title'])
     else:
         print(f"Status Code: {response.status_code}")
 
 
 def fetch_and_save_posts():
-    url = "https://jsonplaceholder.typicode.com/todos/1"
+    url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
@@ -28,3 +29,6 @@ def fetch_and_save_posts():
             writer.writerow(["key", "value"])
             for key, value in data.items():
                 writer.writerow([key, value])
+    else:
+        print(f"Status Code: {response.status_code}")
+fetch_and_print_posts()
