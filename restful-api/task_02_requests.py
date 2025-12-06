3d8611691059370afbca603095050ae362e0cb82
ruhanfarajov <ruhanfarajov@gmail.com>
2025-12-06 21:39:58 +0400

    Update task_02_requests.py

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -12,7 +12,6 @@ def fetch_and_print_posts():
     print(response.status_code)
     if response.status_code == 200:
         data = response.json()
-        print(data)
         for i in data:
             print(i['title'])
     else:
