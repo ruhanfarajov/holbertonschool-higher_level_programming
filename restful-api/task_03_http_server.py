630f9388620a4e86eda8c188392752015ac52c87
ruhanfarajov <ruhanafarajov@gmail.com>
2025-12-06 19:48:08 -0500

    klsdjckdsc

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -49,7 +49,7 @@ class Service(BaseHTTPRequestHandler):
         self.wfile.write(bytes(status_message, 'utf-8'))
 
     def error_handler(self):
-        error_message = f"404 Not Found"
+        error_message = "404 Not Found"
         self.send_response(404)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
